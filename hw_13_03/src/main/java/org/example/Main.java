39d1d4383d73ed4db0f1ed836ffa5e4ff71119b6
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2026-03-14 14:45:04 +0200

    Add files via upload

---
+++ hw_13_03/src/main/java/org/example/Main.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+public class Main {
+
+    static class FileInfo {
+        long count = 0;
+        long size = 0;
+    }
+
+    static Map<String, FileInfo> map = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+
+        Path startPath = Paths.get("D:\\source\\repos\\test");
+
+        Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+
+                File f = file.toFile();
+                String name = f.getName();
+                String ext = "no_ext";
+
+                int dot = name.lastIndexOf(".");
+                if (dot != -1 && dot != name.length() - 1) {
+                    ext = name.substring(dot + 1).toLowerCase();
+                }
+
+                FileInfo info = map.getOrDefault(ext, new FileInfo());
+                info.count++;
+                info.size += f.length();
+
+                map.put(ext, info);
+
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        List<Map.Entry<String, FileInfo>> list = new ArrayList<>(map.entrySet());
+        list.sort((a, b) -> Long.compare(b.getValue().count, a.getValue().count));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"));
+        writer.write(String.format("%-5s %-12s %-12s %-12s\n", "№", "расширение", "количество", "объём"));
+
+        int index = 1;
+
+        for (Map.Entry<String, FileInfo> entry : list) {
+
+            if (index > 50) break;
+            writer.write(String.format("%-5d %-12s %-12d %-12d\n", index, entry.getKey(), entry.getValue().count, entry.getValue().size));
+            index++;
+        }
+        writer.close();
+
+        System.out.println("Готово. Результат в файле result.txt");
+    }
+}
